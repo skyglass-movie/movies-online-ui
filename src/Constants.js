bf644a20e51c9365e5968f7fef2fbb3981fe593d
skyglass <skyglass2001@gmail.com>
2023-05-22 22:06:26 +0200

    initial commit

---
+++ src/Constants.js
@@ -0,0 +1,19 @@
+const prod = {
+  url: {
+    KEYCLOAK_BASE_URL: "https://movie.greeta.net",
+    API_BASE_URL: 'https://movie.greeta.net/movies-api',
+    OMDB_BASE_URL: 'https://www.omdbapi.com',
+    AVATARS_DICEBEAR_URL: 'https://avatars.dicebear.com/api'
+  }
+}
+
+const dev = {
+  url: {
+    KEYCLOAK_BASE_URL: "https://movie.greeta.net",
+    API_BASE_URL: 'https://movie.greeta.net/movies-api',
+    OMDB_BASE_URL: 'https://www.omdbapi.com',
+    AVATARS_DICEBEAR_URL: 'https://avatars.dicebear.com/api'
+  }
+}
+
+export const config = process.env.NODE_ENV === 'development' ? dev : prod
